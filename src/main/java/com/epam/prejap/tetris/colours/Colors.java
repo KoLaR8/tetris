7f68d6ead1cb54d8744afffc9414dd498e7a6eb9
Wojciech Kolarczyk <wkolarczyk99@gmail.com>
2021-06-12 20:50:24 +0200

    add byte parameter to constructor, add id() method to enum Colors

--- src/main/java/com/epam/prejap/tetris/colours/Colors.java
+++ src/main/java/com/epam/prejap/tetris/colours/Colors.java
@@ -1,25 +1,30 @@
-package com.epam.prejap.tetris.game;
+package com.epam.prejap.tetris.colours;
 
 /**
  *
  */
 enum Colors {
-    RESET("\u001B[0m"),
-    RED("\u001B[30m"),
-    GREEN("\u001B[32m"),
-    YELLOW("\u001B[33m"),
-    BLUE("\u001B[34m"),
-    PURPLE("\u001B[35m"),
-    CYAN("\u001B[36m"),
-    WHITE("\u001B[37m");
+    RESET("\u001B[0m", (byte)1),
+    RED("\u001B[30m", (byte)2),
+    GREEN("\u001B[32m", (byte)3),
+    YELLOW("\u001B[33m", (byte)4),
+    BLUE("\u001B[34m", (byte)5),
+    PURPLE("\u001B[35m", (byte)6),
+    CYAN("\u001B[36m", (byte)7),
+    WHITE("\u001B[37m", (byte)8);
 
     private final String asciCode;
+    private final byte id;
 
-    Colors(String asciCode){
+    Colors(String asciCode, byte id){
         this.asciCode = asciCode;
+        this.id = id;
     }
+
     String getCode(){
         return asciCode;
     }
+
+    byte id(){return id;}
 
 }
